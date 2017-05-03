17e9211fa5574ef3f36dd15e76dd8a6a60b80767
Ali Yahya <amyahya@gmail.com>
2017-05-03 14:33:20 -0400

    Add matcher.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -2,4 +2,8 @@ if defined?(ChefSpec)
   def install_chef_rvm(user)
     ChefSpec::Matchers::ResourceMatcher.new(:chef_rvm, :install, user)
   end
+
+  def install_chef_rvm_ruby(user)
+    ChefSpec::Matchers::ResourceMatcher.new(:chef_rvm_ruby, :install, user)
+  end
 end
